f2eb4d67a55055252069695dea723c465066b222
Udazi Ephraim Oseiwe <udazi.osebest@gmail.com>
2023-05-09 08:21:13 +0100

    Made some changes

--- netlify-server/src/api.js
+++ netlify-server/src/api.js
@@ -1,23 +1,19 @@
 import express from "express";
 import serverless from "serverless-http";
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-
-import dalleRoutes from "./routes/dalle.routes.js";
+import cors from "cors";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
 // Create an instance of the Express app
 const app = express();
 
+// Enable CORS for all requests
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-
-app.use('/.netlify/functions/api/dalle', dalleRoutes);
+app.use(express.json({ limit: "50mb" }));
 
 // Create a router to handle routes
 const router = express.Router();
-const router2 = express.Router();
 
 // Define a route that responds with a JSON object when a GET request is made to the root path
 router.get("/", (req, res) => {
@@ -26,15 +22,9 @@ router.get("/", (req, res) => {
   });
 });
 
-router2.get("/", (req, res) => {
-  res.json({
-    user: "user"
-  })
-})
 
 // Use the router to handle requests to the `/.netlify/functions/api` path
 app.use(`/.netlify/functions/api`, router);
-app.use(`/.netlify/functions/api/user`, router2);
 
 // Export the app and the serverless function
 module.exports = app;
